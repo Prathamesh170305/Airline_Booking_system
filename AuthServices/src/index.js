937712c0550c0dafe40b67f0947a90016b8adc2f
Prathamesh170305 <wankhadeprathamesh17@gmail.com>
2025-03-18 22:53:11 +0530

    doen with user repo getbyid func

--- AuthServices/src/index.js
+++ AuthServices/src/index.js
@@ -3,6 +3,7 @@ const bodyParser=require('body-parser');
 
 const {PORT}=require('./config/serverConfig');
 const apiRoutes=require('./routes/index');
+//const UserRepository=require('./repository/user-repository');
 
 const app = express();
 
@@ -12,8 +13,11 @@ const prepareAndStart=()=>{
 
     app.use('/api',apiRoutes);
 
-    app.listen(PORT,()=>{
+    app.listen(PORT, async ()=>{
         console.log(`Server started on port:${PORT}`);
+        // const repo=new UserRepository();
+        // const response=await repo.getById(1);
+        // console.log(response);
         
     })
 }
