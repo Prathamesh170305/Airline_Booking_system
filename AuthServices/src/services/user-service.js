df33380b7fed7ffce4e4f7a7d2aad52bfcf70c89
Prathamesh170305 <wankhadeprathamesh17@gmail.com>
2025-03-18 23:22:27 +0530

    Added function to compare the incoming password with encrypted password

--- AuthServices/src/services/user-service.js
+++ AuthServices/src/services/user-service.js
@@ -1,18 +1,50 @@
-const UserRepository=require('../repository/user-repository');
+const jwt = require('jsonwebtoken');
+const bcrypt=require('bcrypt');
 
-class UserService{
-    constructor(){
-        this.userRepository=new UserRepository();
+const UserRepository = require('../repository/user-repository');
+const { JWT_KEY } = require('../config/serverConfig');
+class UserService {
+    constructor() {
+        this.userRepository = new UserRepository();
     }
 
-    async create(data){
+    async create(data) {
         try {
-            const user=await this.userRepository.create(data);
+            const user = await this.userRepository.create(data);
             return user;
         } catch (error) {
             console.log('something went wrong in the service layer');
             throw error;
         }
     }
+
+    createToken(user) {
+        try {
+            const result = jwt.sign(user, JWT_KEY, { expiresIn: '1h' });
+            return result;
+        } catch (error) {
+            console.log('something went wrong in the service layer (token creation)');
+            throw error;
+        }
+    }
+
+    verifyToken(token){
+        try {
+            const response =jwt.verify(token,JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log('something went wrong in the service layer (token verification)');
+            throw error;
+        }
+    }
+
+    checkPassword(userInputPlainPassword,encryptedPassword){
+        try {
+            return bcrypt.compareSync(userInputPlainPassword,encryptedPassword);
+        } catch (error) {
+            console.log('something went wrong in the password verification');
+            throw error;
+        }
+    }
 }
-module.exports=UserService;
+module.exports = UserService;
