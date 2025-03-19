dea2cbf7c46bfb4091f9539342b099c64d591e12
Prathamesh170305 <wankhadeprathamesh17@gmail.com>
2025-03-19 23:45:50 +0530

    added the roles and the user-roles

--- AuthServices/src/index.js
+++ AuthServices/src/index.js
@@ -4,6 +4,9 @@ const bodyParser=require('body-parser');
 const {PORT}=require('./config/serverConfig');
 const apiRoutes=require('./routes/index');
 //const UserRepository=require('./repository/user-repository');
+const db=require('./models/index');
+// const {User,Role}=require('./models/index');
+// const user = require('./models/user');
 
 const app = express();
 
@@ -15,6 +18,14 @@ const prepareAndStart=()=>{
 
     app.listen(PORT, async ()=>{
         console.log(`Server started on port:${PORT}`);
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter:true});
+        }
+
+        // const u1=await User.findByPk(1);
+        // const r1=await Role.findByPk(1);
+        // u1.addRole(r1);
+
         // const repo=new UserRepository();
         // const response=await repo.getById(1);
         // console.log(response);
